7e68b42f525da7c5ef0de80c295e520c8e2b7164
Jessica Scarborough <jess.scarborough12@gmail.com>
2017-05-07 15:16:29 -0700

    Add files with names changed The differnt names allow for easier command line operation.

---
+++ script.R
@@ -0,0 +1,9 @@
+#### Data Simulation ####
+
+set.seed(0)
+# Age
+
+age.6_11 <- rnorm(n=134, mean=8.99, sd=1.82)
+age.12_18 <- rnorm(n=215, mean=14.40, sd=2.19)
+age.19_25 <- rnorm(n=179, mean=21.94, sd=2.07)
+age.26_36 <- rnorm(n=187, mean=30.64, sd=3.07)
